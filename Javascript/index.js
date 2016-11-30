94a39a98bf046f6929c9a93c5c84da07c31edd16
Adriboz <caboteur.adrienblanchot@gmail.com>
2016-11-30 10:56:01 +0100

    première version

---
+++ Javascript/index.js
@@ -0,0 +1,26 @@
+var resultat = [inputTVA,inputValue,inputType];
+var convertisseur = {
+  inputTVA:20,
+  inputValue:10,
+  inputType:"ht",
+};
+var click = fonction () {
+
+};
+var convert = fonction (param) {
+param.inputTVA
+param.inputValue
+param.inputType
+}
+
+var nombre1= prompt ("ht ou ttc");
+var nombre2 = prompt('valeur de la tva en pourcentage ?');
+var nombre3 = prompt('valeur à convertir ?');
+
+
+
+var resultat = {
+if (nombre1==="ht") {nombre3+(nombre3*nombre2/100);}
+else {nombre3-(nombre3*nombre2/100)}
+ };
+alert(resultat)
